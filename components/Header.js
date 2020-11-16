799b734f29387cd61649d80a407d0260100f9d50
pat-white-code <pat.k.white@gmail.com>
2020-11-16 14:11:08 -0600

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import Nav from './Nav';
+
+const Header = () => 
+  (
+    <div>
+      <div className="bar">
+        <a>Jdarwish Fitness</a>
+        <Nav />
+      </div>
+    </div>
+  )
+
+
+export default Header;
---
+++ pages/index.js
@@ -0,0 +1,31 @@
+const newUser = () => {
+  return(
+    <div>
+      <h1>
+        Jdarwish Fitness
+      </h1>
+      <h2>
+        Full Access to all of my content
+      </h2>
+      <ul>
+        <li>
+          Unlock the full workout experience
+        </li>
+        <li>
+          Watch step by step video instruction
+        </li>
+        <li>
+          Track Your reps, weight and more
+        </li>
+      </ul>
+      <div>
+        <button>Annual</button>
+        <button>Monthly</button>
+        <p>See Terms and conditions</p>
+        <button>Start Free Trial</button>
+      </div>
+    </div>
+  )
+}
+
+export default newUser;
